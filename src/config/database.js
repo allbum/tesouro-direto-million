753541084230e3cbdf32d4128a755a8e7125009c
AllanEP <allanbum@gmail.com>
2020-05-07 21:49:06 -0300

    config variavel ambiente e port padrao

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost:27017/tesouroMillion';
 
-const uri = 'mongodb://localhost:27017/tesouroMillion';
-
+//const uri = 
 mongoose.connect(uri,  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 mongoose.Promise = global.Promise;
 
